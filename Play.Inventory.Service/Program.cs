e19aac2bc155aedac60f56719fc591e8bd7f08fd
Luciano Correia <luciano.correia@gmail.com>
2023-11-03 13:16:58 +0400

    update cors

--- Play.Inventory.Service/Program.cs
+++ Play.Inventory.Service/Program.cs
@@ -34,6 +34,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseCors(builder => builder
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin());
 }
 
 app.UseHttpsRedirection();
